075d85f37ba8afd2650f0235274a3c22c66b9e95
mads-b <=>
2013-02-27 13:50:00 +0100

    Fix for edvin

--- src/com/eit/minimap/MapActivity.java
+++ src/com/eit/minimap/MapActivity.java
@@ -1,33 +1,19 @@
 package com.eit.minimap;
 
-import android.app.ActionBar;
 import android.app.Activity;
-import android.app.AlertDialog;
-import android.app.Dialog;
-import android.app.ProgressDialog;
 import android.content.Context;
-import android.content.DialogInterface;
-import android.content.Intent;
 import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationManager;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuInflater;
-import com.eit.minimap.network.JsonTcpClient;
+import com.eit.minimap.datastructures.UserStore;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
 
-import java.io.IOException;
-import java.net.InetAddress;
-import java.net.UnknownHostException;
-
-public class MapActivity extends Activity {
+public class MapActivity extends Activity implements UserStore.UserStoreListener {
     private GoogleMap map;
 
     private final static String TAG = "com.eit.minimap.MapActivity";
@@ -38,6 +24,11 @@ public class MapActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // The following three lines activates the GPS and connects to server.
+        //UserStore users = new UserStore(this);
+        //users.registerListener(this);
+        //users.init();
+
         setContentView(R.layout.map);
         final MapFragment mapFrag = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
         map = mapFrag.getMap();
@@ -49,9 +40,15 @@ public class MapActivity extends Activity {
         crit.setAccuracy(Criteria.NO_REQUIREMENT);
         final String provider = lm.getBestProvider(crit, true);
         final Location loc = lm.getLastKnownLocation(provider);
-        Log.d(TAG,"AnyLocation: "+loc.getLatitude()+" x "+loc.getLongitude()+"Vis?: "+mapFrag.isVisible());
-        map.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(loc.getLatitude(),loc.getLongitude())));
-        map.animateCamera(CameraUpdateFactory.zoomBy(15f),5000,null);
+        if(loc != null) {
+            Log.d(TAG,"AnyLocation: "+loc.getLatitude()+" x "+loc.getLongitude()+"Vis?: "+mapFrag.isVisible());
+            map.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(loc.getLatitude(),loc.getLongitude())));
+            map.animateCamera(CameraUpdateFactory.zoomBy(15f),5000,null);
+        }
     }
 
+    @Override
+    public void storeChanged(UserStore store) {
+        //TODO: Implement
+    }
 }
