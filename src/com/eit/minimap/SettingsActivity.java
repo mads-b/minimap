30faa35da22c34dc25cae4d00b9b6c66abf83b65
Edvin Hatlevik <makalaust@hotmail.com>
2013-03-13 13:32:33 +0100

    Made more changes to the settings menu

--- src/com/eit/minimap/SettingsActivity.java
+++ src/com/eit/minimap/SettingsActivity.java
@@ -1,31 +1,27 @@
 package com.eit.minimap;
 
-import android.app.ActionBar;
 import android.app.Activity;
-import android.content.Context;
-import android.location.Criteria;
-import android.location.Location;
-import android.location.LocationManager;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
-import com.eit.minimap.datastructures.Coordinate;
-import com.eit.minimap.datastructures.User;
-import com.eit.minimap.datastructures.UserStore;
-import com.eit.minimap.network.NetworkListener;
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapFragment;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.android.gms.maps.model.PolylineOptions;
+import android.preference.PreferenceManager;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 
-public class SettingsActivity extends Activity {
 
-    private final static String TAG = "com.eit.minimap.SettingsActivity";
+public class SettingsActivity extends Activity implements View.OnClickListener{
 
+    private SharedPreferences preferences;
+	private EditText yourName;
+	private EditText groupName;
+	private EditText iconName;
+	private EditText serverAdr;
+	private CheckBox viewStaus;
+	private CheckBox viewMessges;
+	private CheckBox viewUsers;
+
+    
     /**
      * Called when the activity is first created.
      */
@@ -33,5 +29,42 @@ public class SettingsActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings_menu);
+        Button ok = (Button) findViewById(R.id.ok_button);
+        ok.setOnClickListener(this);
+        preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        
+        // Get Fields
+        groupName = (EditText) findViewById(R.id.group_name);
+        yourName = (EditText) findViewById(R.id.your_name);
+        iconName = (EditText) findViewById(R.id.icon_name);
+        serverAdr = (EditText) findViewById(R.id.server_adr);
+        viewStaus = (CheckBox) findViewById(R.id.view_status);
+        viewMessges = (CheckBox) findViewById(R.id.view_messges);
+        viewUsers = (CheckBox) findViewById(R.id.view_users);
+        
+        // Setting text from preferences to fields
+        yourName.setText(preferences.getString("groupName",""));
+        groupName.setText(preferences.getString("yourName",""));
+        iconName.setText(preferences.getString("iconName","Medic"));
+        viewStaus.setChecked(preferences.getBoolean("viewStaus",true));
+        viewMessges.setChecked(preferences.getBoolean("viewMessges",true));
+        viewUsers.setChecked(preferences.getBoolean("viewMessges",false));
+
     }
+
+	@Override
+	public void onClick(View v) {
+		if(v.getId() == R.id.ok_button){
+			// Update preferences from updated fields
+	        preferences.edit()
+            .putString("groupName",this.yourName.getText().toString())
+            .putString("yourName",this.groupName.getText().toString())
+            .putString("iconName",this.iconName.getText().toString())
+            .putString("serverAdr",this.serverAdr.getText().toString())
+            .putBoolean("viewStaus", this.viewStaus.isChecked())
+            .putBoolean("viewMessges", this.viewStaus.isChecked())
+            .putBoolean("viewUsers", this.viewStaus.isChecked())
+            .commit();
+		}
+	}
 }
